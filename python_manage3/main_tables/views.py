70ebb7d63e5c16fa1fcd7ac216bb278979b1a47b
Tony Carballeira <tonycarballeira@gmail.com>
2015-12-16 15:06:13 -0500

    Used raw SQL for user select query in view

--- python_manage3/main_tables/views.py
+++ python_manage3/main_tables/views.py
@@ -10,6 +10,8 @@ from .forms import SysSyaAccountForm
 
 import Cookie
 
+from django.db import connection
+
  
 
 # Create your views here.
@@ -27,10 +29,18 @@ def sign_in(request):
 		u_name = request.POST.get("sya_name")
 		u_password = request.POST.get("sya_password")
 		form = SysSyaAccountForm(initial={"sya_name":"%(u_name)s" % { "u_name":u_name}, "sya_password":"%(u_password)s" % {"u_password":u_password} })
-		query = SysSyaAccount.objects.filter(sya_name="%(u_name)s" % {"u_name":u_name}).filter(sya_password="%(u_password)s" % {"u_password":u_password})
-		length = len(query)
 
-		if length > 0:
+		#::USING RAW SQL::
+		cursor = connection.cursor()
+		sql_query = cursor.execute("SELECT * FROM  sys_sya_account WHERE sya_name=%s AND sya_password=%s", (u_name, u_password,)) 
+		rows = sql_query.fetchall()
+		rows_length = len(rows)
+
+		#::USING ORM::
+		# query = SysSyaAccount.objects.filter(sya_name="%(u_name)s" % {"u_name":u_name}).filter(sya_password="%(u_password)s" % {"u_password":u_password})
+		# length = len(query)
+
+		if rows_length > 0:
 			response = HttpResponseRedirect("sign_in", locals())
 			response.set_cookie("new_cook", "signed_in", max_age = 50000)
 			return response
@@ -46,8 +56,6 @@ def sign_in(request):
 		return render_to_response("signin.html",								 
 								   locals(), 
 								   context_instance=RequestContext(request))
-	
-
 
 
 
